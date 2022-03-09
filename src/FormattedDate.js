ff5bee1207fa24c9405c6c3542b868886f0ed60c
lauragomez04 <lauragomezb0904@gmail.com>
2022-03-09 18:33:50 +0000

    set the date

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -1,12 +1,42 @@
 import React from "react";
 
 export default function FormattedDate(props) {
-  //here my props.date is undefined and I cant continue editing  my date //
+  let days = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
+  let day = days[props.date.getDay()];
+  let hours = props.date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let minutes = props.date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let dayDate = props.date.getDate();
+  let months = [
+    "jan",
+    "feb",
+    "mar",
+    "apr",
+    "jun",
+    "jul",
+    "aug",
+    "sep",
+    "oct",
+    "nov",
+    "dec",
+  ];
+  let month = months[props.date.getMonth()];
 
   return (
     <div className="FormattedDate">
-      <div className="time">18:06</div>
-      <div className="date">sat, 19 feb.</div>
+      <div className="time">
+        {hours}:{minutes}
+      </div>
+      <div className="date">
+        {day}, {dayDate} {month}.
+      </div>
     </div>
   );
 }
